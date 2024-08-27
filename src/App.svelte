bf7c2859f9b41ca6be1a308fb7b8d434a9bfb485
Shotaro Nakamura <syoutarou1103@outlook.jp>
2024-08-27 11:54:15 +0000

    feat: 天気を追加 そろそろダサくなってきた

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
 import Footer from './views/Footer.svelte';
 import RealTimeSindo from './views/RealTimeSindo/index.svelte'
+import MainContent from './views/MainContent/index.svelte'
+    import Weather from './views/Weather.svelte';
 </script>
 <div class="flex flex-col h-dvh">
-  <div class="grow">
-    content content
+  <div class="grow flex">
+    <div class="grow">
+      <MainContent />
+    </div>
+    <div class="border-l p-1 h-full">
+      <Weather />
+    </div>
   </div>
   <hr />
   <div>
---
+++ src/views/Weather.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  let url = 'https://www.data.jma.go.jp/fcd/yoho/data/wxchart/quick/202408/SPAS_COLOR_202408261800.png'
+</script>
+<div class="h-full grid grid-rows-2">
+  <div>
+    <div class="grid grid-cols-2 place-items-start">
+      <div class="bg-red-200 p-2 text-xl rounded-full">怪獣警報</div>
+      <div class="">
+        <div>東京都</div>
+        <div>神奈川県</div>
+      </div>
+    </div>
+  </div>
+  <div>
+    <img class="w-64 h-64" src={`https://corsproxy.io/?${url}`} alt="天気図" />
+  </div>
+</div>
